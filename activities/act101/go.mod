3eb0738e2b22174832036ac8a3c41564192f5124
ProSweeper <dylan.doucet28@gmail.com>
2024-06-12 11:31:38 -0600

    work

---
+++ activities/act101/go.mod
@@ -0,0 +1,3 @@
+module act101
+
+go 1.22.4
---
+++ activities/act101/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var firstName string = "Dylan"
+var familyName string = "Doucet"
+var age int = 27
+var peanutAllergy bool = false
+
+func main() {
+	fmt.Println(firstName)
+	fmt.Println(familyName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
